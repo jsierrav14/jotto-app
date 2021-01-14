9abc81de83732d3c609470dc98d470538244449e
Jose Eduardo Sierra Vargas <jsierrav14@gmail.com>
2021-01-14 09:45:23 -0500

    adding congrats component tests

---
+++ Congrat.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+export default(props)=>{
+
+        if(props.success){
+            return(
+            <div data-test="component-congrats">
+            <span data-test="congrats-message">
+            Congratulations! you guessed the wordß
+            
+            </span>
+            </div>)
+        }else{
+            return(
+                <div data-test="component-congrats" />
+            )
+        }
+    
+}
